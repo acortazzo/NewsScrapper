5d27e09aa4288e3c35335c17ddd095fe783bba49
acortazzo <alejandro.cortazzo@gmail.com>
2021-04-15 12:47:59 -0300

    first commit

---
+++ newsScrapper.py
@@ -0,0 +1,8 @@
+# 1. Descargar html pasando una url
+    #TODO: Eliminar htmls viejos
+
+# 2. "scrapear" html y dejar solo el body 
+
+# 3. "Printear" el body depurado, y meterlo en otro html
+
+# 4. Abrir el html 
